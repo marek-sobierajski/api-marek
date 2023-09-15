334539157dc9874edf90f02dbe490f54dbf1eb29
marek-sobierajski <marek-sobierajski@wp.pl>
2023-09-15 21:27:56 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/post_endpoint', methods=['POST'])
+@app.route('/post_endpoint', methods=['POST', 'GET'])  # Added 'GET' to the methods list
 def post_example():
-    data = request.json
-    print("Data received:", data)
-    return jsonify({"message": "Data received", "data": data})
+    if request.method == 'POST':
+        data = request.json
+        print("Data received:", data)
+        app.logger.info(data)
+        return jsonify({"message": "Data received", "data": data})
+    elif request.method == 'GET':
+        return jsonify({"message": "GET request received"})
 
 if __name__ == '__main__':
     app.run(debug=True)
